ecc88721dd7f42ced15175afc9f895420952df45
MitsukiIwamura <boc.fsfb@gmail.com>
2022-10-05 00:45:48 +0900

    relay insstall

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../lib/prisma";
 
 export default NextAuth({
