86d34282443779927798364feada5c180cfdf7cf
MitsukiIwamura <boc.fsfb@gmail.com>
2022-10-12 23:18:36 +0900

    relay導入

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  optimizeFonts: true,
-};
+const webpack = require('webpack')
 
-module.exports = nextConfig;
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
+    optimizeFonts: true,
+    webpack: (config, { isServer, webpack }) => {
+        if (!isServer) {
+            // Ensures no server modules are included on the client.
+            config.plugins.push(
+                new webpack.IgnorePlugin({ resourceRegExp: /lib\/server/ })
+            )
+        }
+
+        return config
+    },
+}
