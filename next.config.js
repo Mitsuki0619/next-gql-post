94c190bb93f5bb52c09c2ec377a50ba713fe0705
MitsukiIwamura <boc.fsfb@gmail.com>
2022-10-16 14:29:18 +0900

    relay アンインストール

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-const webpack = require('webpack')
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    optimizeFonts: true,
-    webpack: (config, { isServer, webpack }) => {
-        if (!isServer) {
-            // Ensures no server modules are included on the client.
-            config.plugins.push(
-                new webpack.IgnorePlugin({ resourceRegExp: /lib\/server/ })
-            )
-        }
-
-        return config
-    },
 }
+
+module.exports = nextConfig
--- relay.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    src: './',
-    schema: './schema/__generated__/schema.graphql',
-    exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
-    extensions: ['ts', 'tsx'],
-    language: 'typescript',
-    artifactDirectory: 'queries/__generated__',
-}
